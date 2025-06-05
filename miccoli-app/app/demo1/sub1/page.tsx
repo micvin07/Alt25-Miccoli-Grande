56e6fb7b4c30fd285ffdaca255b5270728447fd8
micvin07 <vincenzo.miccoli@itisandria.it>
2025-06-05 11:58:11 +0200

    lezione 2.04

---
+++ miccoli-app/app/demo1/sub1/page.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Demo1/Sub1 - Miccoli App",
+  description: "Questa è la pagina Demo1/Sub1 dell'app di Miccoli.",
+  openGraph: {
+    title: "Demo1/Sub1 - Miccoli App",
+    description: "Questa è la pagina Demo1/Sub1 dell'app di Miccoli.",
+  }
+}
+
+export default function Sub1() {
+  return (
+    <main className="page">
+      <h1>Sub1</h1>
+    </main>
+  );
+}
---
+++ miccoli-app/app/demo1/sub2/page.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Demo1/Sub2 - Miccoli App",
+  description: "Questa è la pagina Demo1/Sub2 dell'app di Miccoli.",
+  openGraph: {
+    title: "Demo1/Sub2 - Miccoli App",
+    description: "Questa è la pagina Demo1/Sub2 dell'app di Miccoli.",
+  }
+}
+
+export default function Sub2() {
+  return (
+    <main className="page">
+      <h1>Sub2</h1>
+    </main>
+  );
+}

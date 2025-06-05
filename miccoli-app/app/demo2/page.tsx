e3a269899bf693cbbe35516403fc91c7c7db2cf8
micvin07 <vincenzo.miccoli@itisandria.it>
2025-06-05 15:09:38 +0200

    lezione 3.05

--- miccoli-app/app/demo2/page.tsx
+++ miccoli-app/app/demo2/page.tsx
@@ -8,8 +8,9 @@ export const metadata = {
 }
 
 import { User } from '../../model/user';
+import { Post } from '../../model/post';
 
-async function getData(): Promise<User[]> {
+async function getUsers(): Promise<User[]> {
   const res = await fetch('https://jsonplaceholder.typicode.com/users', {
     cache: 'no-store'
   })
@@ -17,15 +18,32 @@ async function getData(): Promise<User[]> {
   return res.json();
 }
 
+async function getPosts(): Promise<Post[]> {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  // Simulate Latency
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  return res.json();
+}
+
 export default async function Demo2() {
-  const data = await getData();
+  const users = await getUsers();
+  const posts = await getPosts();
 
   return (
     <main>
       <h1>Demo 2</h1>
+
+      <h2>Users</h2>
       {
-        data?.map(user => {
+        users?.map(user => {
           return <li key={user.id}>{user.name}</li>
+        })
+      }
+
+      <h2>Posts</h2>
+      {
+        posts?.map(post => {
+          return <li key={post.id}>{post.title}</li>
         })
       }
     </main>

46fe1282bca82f41c95991c4d15496edefc3b628
micvin07 <vincenzo.miccoli@itisandria.it>
2025-06-05 12:31:09 +0200

    lezione 2.05

---
+++ miccoli-app/app/demo1/layout.tsx
@@ -0,0 +1,15 @@
+export default function Demo1Layout({
+ children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div lang="it">
+      <a href="/demo1">Demo1</a>
+      <a href="/demo1/sub1">Sub1</a>
+      <a href="/demo1/sub2" >Sub2</a>
+      <hr/>
+      {children}
+    </div>
+  )
+}
--- miccoli-app/app/demo1/page.tsx
+++ miccoli-app/app/demo1/page.tsx
@@ -10,7 +10,9 @@ export const metadata = {
 export default function Demo1() {
   return (
     <main className="page">
-      <h1>Demo1</h1>
+
+        <h1>Demo1</h1>
+
     </main>
   );
 }
